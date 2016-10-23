ee60c884458a2bfef9049f928419a32b879269d2
iWynn <huainian@mail.com>
2016-10-24 04:27:04 +0800

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -22,7 +22,7 @@ map_manager.map_items = [
 
 function get_counter_down_time_from_expire_epoch(epoch) {
     var now_time = new Date().getTime()/1000;
-    var time_left = epoch - now_time;
+    var time_left = epoch / 1000 - now_time;
     var second = Math.floor(time_left % 60);
     var minute = Math.floor(time_left / 60);
     return minute + ":" + second;
